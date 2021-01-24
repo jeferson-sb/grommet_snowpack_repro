2286c7b8eac0a7f9f30cb933c4300ebed080adf8
Jeferson Brito <jeferson.brito@codeminer42.com>
2021-01-23 22:27:18 -0300

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Grommet, Box, Button } from 'grommet';
 
-function App() {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+    <Grommet plain>
+      <Box align="center">
+        <Button
+          as="a"
+          href="https://v2.grommet.io/"
+          target="_blank"
+          rel="noopener noreferrer"
+          label="Get Started"
+          primary
+        />
+      </Box>
+    </Grommet>
   );
 }
-
-export default App;
